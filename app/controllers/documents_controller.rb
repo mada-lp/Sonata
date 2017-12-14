31771ff687a61f7f46d9ae651ecae082cfff1383
Madalena Pessanha <madalena.lpessanha@gmail.com>
2017-12-14 20:44:51 -0200

    controller crud ok

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -10,6 +10,7 @@ class DocumentsController < ApplicationController
   # GET /documents/1
   # GET /documents/1.json
   def show
+    @document = Document.find(params[:id])
   end
 
   # GET /documents/new
@@ -17,9 +18,6 @@ class DocumentsController < ApplicationController
     @document = Document.new
   end
 
-  # GET /documents/1/edit
-  def edit
-  end
 
   # POST /documents
   # POST /documents.json
@@ -28,36 +26,12 @@ class DocumentsController < ApplicationController
 
     respond_to do |format|
       if @document.save
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
+        format.html { redirect_to @document, notice: 'Documento criado com sucesso.' }
         format.json { render :show, status: :created, location: @document }
       else
         format.html { render :new }
         format.json { render json: @document.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /documents/1
-  # PATCH/PUT /documents/1.json
-  def update
-    respond_to do |format|
-      if @document.update(document_params)
-        format.html { redirect_to @document, notice: 'Document was successfully updated.' }
-        format.json { render :show, status: :ok, location: @document }
-      else
-        format.html { render :edit }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /documents/1
-  # DELETE /documents/1.json
-  def destroy
-    @document.destroy
-    respond_to do |format|
-      format.html { redirect_to documents_url, notice: 'Document was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
